caa9e200bf88c89e7d69008d3aadfdba850cb66f
Dotan <dotantz26@gmail.com>
2021-05-24 00:02:53 +0300

    update screenshot and app

--- src/App.tsx
+++ src/App.tsx
@@ -84,7 +84,7 @@ const App = () => {
   return (
     <div className="page-container">
       <div className="content-container">
-        
+
         <div className="form-area">
           <div className="input-area">
             <input type="text" className="email-input" placeholder="Email" value={email} onChange={(e) => { setEmail(e.target.value) }}></input>
